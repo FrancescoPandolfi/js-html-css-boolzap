7abc4d4690fa57e650a29d9526d0d74ac4fbde85
Francesco Pandolfi <francesco.pan@gmail.com>
2020-01-24 00:18:31 +0100

    Sistemato messaggio e aggiunta freccia sull'hover dei messaggi

--- js/script.js
+++ js/script.js
@@ -1,23 +1,39 @@
 $(document).ready(function () {
 
-  $(".mex").focus(function() {
-    $('.vocal-icon').hide();
-    $('.send-icon').show();
+$(".mex").focus();
+
+
+  $('.mex').on('keyup', function() {
+       if (this.value.length > 0) {
+         $('.vocal-icon').hide();
+         $('.send-icon').show();
+       } else {
+         $('.vocal-icon').show();
+         $('.send-icon').hide();
+       }
   });
 
-  // $(".mex").focusout(function(){
-  //   $('.vocal-icon').show();
-  //   $('.send-icon').hide();
-  // });
 
   $(document).on('click', '.send-icon', function() {
-    var templateCopy = $('.message-window + .template').clone();
+    var templateCopy = $('.message-window + .message-wrapper').clone();
     var typedMessage = $('.mex').val();
 
-    templateCopy.children().text(typedMessage);
+    templateCopy.children('p').text(typedMessage);
     $('.message-window').append(templateCopy);
-    $('.message-window .template').removeClass('d-none');
+    $('.message-window .message-wrapper').removeClass('d-none');
+    $('.mex').val('');
 
+    $('.vocal-icon').show();
+    $('.send-icon').hide();
+  });
+
+  $('.message-wrapper').mouseenter(function() {
+    $(this).children('.arrow-down').addClass('arrow-down-animation');
+    $(this).children('.arrow-down').removeClass('d-none');
+  });
+  $('.message-wrapper').mouseleave(function() {
+    $(this).children('.arrow-down').removeClass('arrow-down-animation');
+    $(this).children('.arrow-down').addClass('d-none');
   });
 
 
