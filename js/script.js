eac8dc27e3d62a95fb5bb2e8e5b772df79d3de69
Francesco Pandolfi <francesco.pan@gmail.com>
2020-01-23 17:01:16 +0100

    Js per l'invio del testo

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 $(document).ready(function () {
 
+  $(".mex").focus(function() {
+    $('.vocal-icon').hide();
+    $('.send-icon').show();
+  });
+
+  // $(".mex").focusout(function(){
+  //   $('.vocal-icon').show();
+  //   $('.send-icon').hide();
+  // });
+
+$(document).on('click', '.send-icon', function() {
+  var myMessage = $('my-message').clone();
+  console.log(myMessage.html());
+
+});
+
+
 
 
 });
