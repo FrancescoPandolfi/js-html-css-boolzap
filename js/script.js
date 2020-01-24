fe6df5a3fcea53bc6a15299b64c22c9acef0610c
Francesco Pandolfi <francesco.pan@gmail.com>
2020-01-24 14:55:18 +0100

    Alcuni aggiornamenti sul layout

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
 
+// Mette il focus sull'input principale appena apri il sito
 $(".mex").focus();
 
-
+  // Nasconde e mostra l'icona per inviare il messaggio
   $('.mex').on('keyup', function() {
        if (this.value.length > 0) {
          $('.vocal-icon').hide();
@@ -13,7 +14,7 @@ $(".mex").focus();
        }
   });
 
-
+  // Azioni che si innescano al click dell'invio messaggio
   $(document).on('click', '.send-icon', function() {
     var templateCopy = $('.message-window + .message-wrapper').clone();
     var typedMessage = $('.mex').val();
@@ -22,11 +23,13 @@ $(".mex").focus();
     $('.message-window').append(templateCopy);
     $('.message-window .message-wrapper').removeClass('d-none');
     $('.mex').val('');
+    $(".mex").focus();
 
     $('.vocal-icon').show();
     $('.send-icon').hide();
   });
 
+  // Azioni che si innescano all'hover del box messaggio inviato
   $('.message-wrapper').mouseenter(function() {
     $(this).children('.arrow-down').addClass('arrow-down-animation');
     $(this).children('.arrow-down').removeClass('d-none');
