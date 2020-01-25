ef17394cb867dbd04a7e8989ed3c4825129afdd7
Francesco Pandolfi <francesco.pan@gmail.com>
2020-01-25 13:41:50 +0100

    Aggiunta possibilità invio messaggio premendo su enter

--- js/script.js
+++ js/script.js
@@ -14,37 +14,49 @@ $(".mex").focus();
        }
   });
 
-  // Azioni che si innescano al click dell'invio messaggio
+
   $(document).on('click', '.send-icon', function() {
-    var messageTemplate = $('.message-window + .message-wrapper').clone();
-    var typedMessage = $('.mex').val();
-
-    var data = new Date();
-    var hours = addZero(data.getHours());
-    var minutes = addZero(data.getMinutes());
-    var time = hours + ':' + minutes;
-
-    messageTemplate.children('p').text(typedMessage);
-    messageTemplate.children('time').text(time);
-
-    $('.message-window').append(messageTemplate);
-    messageTemplate.removeClass('d-none').addClass('sent');
-
-    $('.mex').val('');
-    $(".mex").focus();
-
-    $('.vocal-icon').show();
-    $('.send-icon').hide();
-
-    // Invia un messaggio dopo un secondo
-    setTimeout(function(){
-      var mexReceivedTemplate = $('.message-window + .message-wrapper').clone();
-      mexReceivedTemplate.children('p').text('Che fai di bello? 😀😀😀');
-      mexReceivedTemplate.children('time').text(time);
-      $('.message-window').append(mexReceivedTemplate);
-      mexReceivedTemplate.removeClass('d-none').addClass('received');
-    }, 1000);
+    sendMessage();
   });
+
+  $(".mex").keyup(function(event) {
+    if (event.keyCode === 13 && $(".mex").val().length > 0) {
+        sendMessage();
+    }
+});
+
+// Azioni che si innescano al click dell'invio messaggio
+function sendMessage() {
+  var messageTemplate = $('.message-window + .message-wrapper').clone();
+  var typedMessage = $('.mex').val();
+
+  var data = new Date();
+  var hours = addZero(data.getHours());
+  var minutes = addZero(data.getMinutes());
+  var time = hours + ':' + minutes;
+
+  messageTemplate.children('p').text(typedMessage);
+  messageTemplate.children('time').text(time);
+
+  $('.message-window').append(messageTemplate);
+  messageTemplate.removeClass('d-none').addClass('sent');
+
+  $('.mex').val('');
+  $(".mex").focus();
+
+  $('.vocal-icon').show();
+  $('.send-icon').hide();
+
+  // Invia un messaggio dopo un secondo
+  setTimeout(function(){
+    var mexReceivedTemplate = $('.message-window + .message-wrapper').clone();
+    mexReceivedTemplate.children('p').text('Che fai di bello? 😀😀😀');
+    mexReceivedTemplate.children('time').text(time);
+    $('.message-window').append(mexReceivedTemplate);
+    mexReceivedTemplate.removeClass('d-none').addClass('received');
+  }, 1000);
+};
+
 
 // funzione per aggiungere uno zero sulla data
 function addZero(num) {
@@ -62,6 +74,23 @@ function addZero(num) {
     $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
   });
 });
+
+
+// row3-left-search
+// Nasconde e rivela icona della ricerca
+
+$(".search").focus(function () {
+  $('.icon-input img:first-child').hide();
+  $('.icon-input img:last-child').show().removeClass('d-none');
+  $(".row3-left-search").css( "background", "white" );
+});
+
+$(".search").focusout(function () {
+  $('.icon-input img:first-child').show();
+  $('.icon-input img:last-child').hide();
+  $(".row3-left-search").css( "background", "#F7F7F7" );
+});
+
 
 
 
